b6e4739fa5ce0a6c2525313abeb194cba1e7288e
Aravind Chowdary <kamaravichow@gmail.com>
2021-05-09 13:51:15 +0530

    viewbinding enabled
    to get rid of pain in the ass findViewById

--- app/src/main/java/me/aravi/os/airpol/MainActivity.kt
+++ app/src/main/java/me/aravi/os/airpol/MainActivity.kt
@@ -1,11 +1,18 @@
 package me.aravi.os.airpol
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.aravi.os.airpol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
 }
