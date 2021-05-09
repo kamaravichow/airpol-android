20134b65e5ee13b511802c7b4ed2fccfe6566db0
Aravind Chowdary <kamaravichow@gmail.com>
2021-05-09 14:03:07 +0530

    add retrofit

---
+++ app/src/main/java/me/aravi/os/airpol/App.kt
@@ -0,0 +1,15 @@
+package me.aravi.os.airpol
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+}
